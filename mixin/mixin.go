603282e709028879f8d6fbd54e7fd0c4e7092226
WLBF <lbf1353@live.com>
2020-10-15 16:21:25 +0800

    update mixin endpoint

--- mixin/mixin.go
+++ mixin/mixin.go
@@ -8,9 +8,11 @@ import (
 var Endpoint = oauth2.Endpoint{
 	AuthURL:  "https://mixin.one/oauth/authorize",
 	TokenURL: "https://api.mixin.one/oauth/token",
+	AuthStyle: oauth2.AuthStyleMixin,
 }
 
 var BackUpEndpoint = oauth2.Endpoint{
 	AuthURL:  "https://mixin-www.zeromesh.net/oauth/authorize",
 	TokenURL: "https://mixin-api.zeromesh.net/oauth/token",
+	AuthStyle: oauth2.AuthStyleMixin,
 }
